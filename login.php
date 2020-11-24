4936b1c5324357f933035d8cc93e4e9c755a9ad5
toykam <mwork063@gmail.com>
2020-11-24 15:31:29 +0100

    Changed base url

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 <?php if (isset($_GET['type'])) { ?>
     <?php $type = $_GET['type']; ?>
     <?php if ($type == 'mentor' || $type == 'mentee') { ?>
-        <?php require_once __DIR__.'./pages/forms/login/index.php'; ?>
+        <?php require_once './pages/forms/login/index.php'; ?>
     <?php } else { ?>
         <h2>The page you are looking for is not here</h2>
     <?php } ?>
